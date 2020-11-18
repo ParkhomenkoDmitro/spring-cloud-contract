0f6618d1d45ebe7f1f00afb4fd37856669b43370
Dmytro <dimagit24@gmail.com>
2020-11-18 23:39:45 +0200

    Added projects

---
+++ cdc-provider/src/main/java/com/parkhomenko/cdcprovider/CdcProviderApplication.java
@@ -0,0 +1,13 @@
+package com.parkhomenko.cdcprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdcProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdcProviderApplication.class, args);
+	}
+
+}
---
+++ cdc-provider/src/test/java/com/parkhomenko/cdcprovider/CdcProviderApplicationTests.java
@@ -0,0 +1,12 @@
+package com.parkhomenko.cdcprovider;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdcProviderApplicationTests {
+
+    //	@Test
+    //	void contextLoads() {
+    //	}
+
+}
